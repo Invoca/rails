94b8df2f816cef47305c7300b058b7b3e88dc8e8
Vicente Carrillo <vcarrillo@invoca.com>
2014-07-23 10:50:39 -0700

    active record migrator

--- activerecord/lib/active_record/migration.rb
+++ activerecord/lib/active_record/migration.rb
@@ -564,9 +564,21 @@ module ActiveRecord
   end
 
   class Migrator#:nodoc:
+    def initialize(direction, migrations_paths, target_version = nil)
+      ActiveRecord::Base.establish_connection(self.class.db_configuration)
+      raise StandardError.new("This database does not yet support migrations") unless Base.connection.supports_migrations?
+      Base.connection.initialize_schema_migrations_table
+      @direction, @migrations_paths, @target_version = direction, migrations_paths, target_version
+    end
+
     class << self
       attr_writer :migrations_paths
       alias :migrations_path= :migrations_paths=
+
+      def db_configuration
+        result = ActiveRecord::Base.configurations[Rails.env + "_migration"] || ActiveRecord::Base.configurations[Rails.env]
+        result
+      end
 
       def migrate(migrations_paths, target_version = nil, &block)
         case
