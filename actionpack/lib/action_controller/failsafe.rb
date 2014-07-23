edf4fa0f22cb7e934bc8f445abeae0fea199cbb4
Vicente Carrillo <vcarrillo@invoca.com>
2014-07-23 10:10:29 -0700

    failsafe

--- actionpack/lib/action_controller/failsafe.rb
+++ actionpack/lib/action_controller/failsafe.rb
@@ -1,8 +1,8 @@
-module ActionController
-  class Failsafe
-    def log_failsafe_exception( exception )
-      ExceptionHandling.log_error( exception, "FAILSAFE Status: 500 Internal Server Error" )
-      Rails.logger.flush if Rails.logger.respond_to?(:flush)
-    end
-  end
-end
+# module ActionController
+#   class Failsafe
+#     def log_failsafe_exception( exception )
+#       ExceptionHandling.log_error( exception, "FAILSAFE Status: 500 Internal Server Error" )
+#       Rails.logger.flush if Rails.logger.respond_to?(:flush)
+#     end
+#   end
+# end
