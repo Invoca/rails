e0a252c63b7d335863c61e86a0c2d58d68d56600
Omeed Rabani <orabani@nborabani.local>
2016-06-23 15:52:07 -0700

    Restore methods, edit self.strip_string_parameters arg

--- actionpack/lib/action_controller/base.rb
+++ actionpack/lib/action_controller/base.rb
@@ -179,6 +179,21 @@ module ActionController
       MODULES - modules
     end
 
+    def self.do_not_strip_string_parameters
+      @do_not_strip_string_parameters
+    end
+
+    def self.strip_string_parameters(except:)
+      @do_not_strip_string_parameters = Array(except)
+    end
+
+    def process_with_strip_string_parameters(action, *args)
+      request.do_not_strip_string_parameters = self.class.do_not_strip_string_parameters
+      process_without_strip_string_parameters(action, *args)
+    end
+
+    alias_method_chain :process, :strip_string_parameters
+
     MODULES = [
       AbstractController::Layouts,
       AbstractController::Translation,
--- actionpack/lib/action_dispatch/http/parameters.rb
+++ actionpack/lib/action_dispatch/http/parameters.rb
@@ -5,13 +5,13 @@ module ActionDispatch
   module Http
     module Parameters
       # Returns both GET and POST \parameters in a single hash.
-      attr_accessor :do_not_strip_string_parameters # Invoca patch
+      attr_accessor :do_not_strip_string_parameters
       def parameters
         @env["action_dispatch.request.parameters"] ||= begin
-          @do_not_strip_string_parameters ||= [] # Invoca patch
+          @do_not_strip_string_parameters ||= []
           params = request_parameters.merge(query_parameters)
           params.merge!(path_parameters)
-          strip_string_params!(params) # Invoca patch
+          strip_string_params!(params)
           encode_params(params).with_indifferent_access
         end
       end
@@ -44,7 +44,7 @@ module ActionDispatch
 
     private
 
-      def strip_string_params!(value_to_strip) # Invoca patch
+      def strip_string_params!(value_to_strip)
         case value_to_strip
         when String
           value_to_strip.strip!
