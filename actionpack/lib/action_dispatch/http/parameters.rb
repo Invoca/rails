4a50c9b679189ff368ca4289fbb3fe2f6c38b6d5
Colin Kelley <colindkelley@gmail.com>
2017-07-17 14:19:47 -0700

    add check for \!do_not_strip_string_parameters so we don't call nil.include?

--- actionpack/lib/action_dispatch/http/parameters.rb
+++ actionpack/lib/action_dispatch/http/parameters.rb
@@ -54,7 +54,11 @@ module ActionDispatch
         when String
           value_to_strip.strip!
         when Hash
-          value_to_strip.each { |key, value| strip_string_params!(value) if !key.respond_to?(:to_sym) || !do_not_strip_string_parameters.include?(key.to_sym) }
+          value_to_strip.each do |key, value|
+            if !key.respond_to?(:to_sym) || !do_not_strip_string_parameters || !do_not_strip_string_parameters.include?(key.to_sym)
+              strip_string_params!(value)
+            end
+          end
         when Array
           value_to_strip.each { |value| strip_string_params!(value) }
         end
